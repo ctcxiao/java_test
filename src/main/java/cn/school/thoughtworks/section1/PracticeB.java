59d9fa3387d9c66b45965039972edf64afa44ad4
ctc <1171332103@qq.com>
2018-04-01 12:14:02 +0800

    提交从集合1中选取和集合2中子元素相同的元素

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> sameElements = new ArrayList<>();
+        for (List<String> sonList: collection2) {
+            for (String element1: collection1) {
+                for (String sonElement: sonList) {
+                    if (element1.equals(sonElement)){
+                        sameElements.add(element1);
+                    }
+                }
+            }
+        }
+        return sameElements;
     }
 }

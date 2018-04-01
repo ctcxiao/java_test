b9d5fe775b3b2abff541892eafdb9560d46b3daf
ctc <1171332103@qq.com>
2018-04-01 13:10:45 +0800

    扩展map记相同个数功能，增加支持key:value,key[value]表达式

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,56 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> sameElementsCount = new HashMap<>();
+        for (String element : collection1) {
+            //首先判断是否特殊元素，不然不好判断是否此元素是否已经存在于map中
+            if (isSpecialElement(element)) {
+                addSpecialElements(sameElementsCount, element);
+            } else {
+                addCommonElement(sameElementsCount, element);
+            }
+        }
+        return sameElementsCount;
+    }
 
-        return null;
+
+    private void addCommonElement(Map<String, Integer> sameElementsCount, String element) {
+        if (sameElementsCount.containsKey(element)) {
+            sameElementsCount.put(element, sameElementsCount.get(element) + 1);
+        } else {
+            sameElementsCount.put(element, 1);
+        }
+    }
+
+    private boolean isSpecialElement(String element) {
+        return element.contains("-") || element.contains(":") || element.contains("[");
+    }
+
+    private String[] handleSpecialElement(String element) {
+        if (element.contains("-")) {
+            return element.split("-");
+        } else if (element.contains(":")) {
+            return element.split(":");
+        } else if (element.contains("[")) {
+            String[] tmp_key_value = element.split("\\[");
+            String key = tmp_key_value[0];
+            String value = tmp_key_value[1].split("]")[0];
+            return new String[]{key, value};
+        }
+        return new String[]{};
+    }
+
+    private void addSpecialElements(Map<String, Integer> sameElementsCount, String element) {
+        String[] key_value = handleSpecialElement(element);
+        if (sameElementsCount.containsKey(key_value[0])) {
+            sameElementsCount.put(key_value[0], sameElementsCount.get(key_value[0]) + Integer.valueOf(key_value[1]));
+        } else {
+            sameElementsCount.put(key_value[0], Integer.valueOf(key_value[1]));
+        }
     }
 }

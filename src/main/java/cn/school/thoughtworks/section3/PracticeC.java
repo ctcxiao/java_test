8f6e72088a99210dd69f461ba7c294d3c3c759a2
ctc <1171332103@qq.com>
2018-04-01 13:39:02 +0800

    扩展上一个功能，可以从没有提供个数的集合进行计算

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,40 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
+
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> sameElemMap = countSameElements(collectionA);
+        return minusSameElemCount(sameElemMap, object);
+    }
 
-        return null;
+    private Map<String,Integer> minusSameElemCount(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+        for (String key: collectionA.keySet()) {
+            for (List<String> valueList: object.values()){
+                for (String value: valueList) {
+                    if (key.equals(value)){
+                        int minusCount = collectionA.get(key)/3;
+                        collectionA.put(key, collectionA.get(key)-minusCount);
+                    }
+                }
+            }
+        }
+        return collectionA;
+    }
+
+    private Map<String,Integer> countSameElements(List<String> collection1) {
+        Map<String, Integer> sameElementsCount = new HashMap<>();
+        for (String element: collection1) {
+            if (sameElementsCount.containsKey(element)){
+                sameElementsCount.put(element, sameElementsCount.get(element)+1);
+            } else {
+                sameElementsCount.put(element, 1);
+            }
+        }
+        return sameElementsCount;
     }
 }

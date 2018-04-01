33af41d5fdce42d9b2dad49086f41c8a0a6f60b6
ctc <1171332103@qq.com>
2018-04-01 13:19:09 +0800

    提交选出1集合中和2集合中value属性中元素相同的元素，并数量减去1的功能

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,15 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for (String key: collectionA.keySet()) {
+            for (List<String> valueList: object.values()){
+                for (String value: valueList) {
+                    if (key.equals(value)){
+                        collectionA.put(key, collectionA.get(key)-1);
+                    }
+                }
+            }
+        }
+        return collectionA;
     }
 }

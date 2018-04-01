57a5b78543e805769b9bf30345901178f8defacd
ctc <1171332103@qq.com>
2018-04-01 12:35:31 +0800

    改进map计算相同元素个数功能，增加支持key-value表达

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,26 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> sameElementsCount = new HashMap<>();
+        for (String element: collection1) {
+            if (sameElementsCount.containsKey(element)){
+                sameElementsCount.put(element, sameElementsCount.get(element)+1);
+            } else {
+                //如果从未添加过的元素包含"-"符号，则做分割处理，
+                //example:d-5->key:d,value:5
+                if (element.contains("-")) {
+                    String[] key_value = element.split("-");
+                    sameElementsCount.put(key_value[0], Integer.valueOf(key_value[1]));
+                } else {
+                    sameElementsCount.put(element, 1);
+                }
+            }
+        }
+        return sameElementsCount;
     }
 }

eb8d70a7d8b7252abb4a70af176ef392d25dac0b
ctc <1171332103@qq.com>
2018-04-01 12:19:57 +0800

    提交从集合中选取和map中value值相同的元素功能

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+    List<String> collectSameElements(List<String> collection1, Map<String, List<String>> collection2) {
+        List<String> sameElements = new ArrayList<>();
+        for (String elements1 : collection1) {
+            for (List<String> mapValues : collection2.values()) {
+                for (String value : mapValues) {
+                    if (elements1.equals(value)) {
+                        sameElements.add(elements1);
+                    }
+                }
+            }
+        }
+        return sameElements;
     }
 }

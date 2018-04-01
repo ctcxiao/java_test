7f68d7ae6375afa37a7b35e3c6b243791baa3271
ctc <1171332103@qq.com>
2018-04-01 12:26:48 +0800

    提交计算map中相同元素个数功能

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,20 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> sameElementsCount = new HashMap<>();
+        for (String element: collection1) {
+            if (sameElementsCount.containsKey(element)){
+                sameElementsCount.put(element, sameElementsCount.get(element)+1);
+            } else {
+                sameElementsCount.put(element, 1);
+            }
+        }
+        return sameElementsCount;
     }
 }

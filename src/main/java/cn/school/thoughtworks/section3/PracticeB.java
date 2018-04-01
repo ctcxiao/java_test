01cb15dba374176d7c104017b773a35c119f70f6
ctc <1171332103@qq.com>
2018-04-01 13:22:12 +0800

    更改上一个功能为每满3减一

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,16 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for (String key: collectionA.keySet()) {
+            for (List<String> valueList: object.values()){
+                for (String value: valueList) {
+                    if (key.equals(value)){
+                        int minusCount = collectionA.get(key)/3;
+                        collectionA.put(key, collectionA.get(key)-minusCount);
+                    }
+                }
+            }
+        }
+        return collectionA;
     }
 }
